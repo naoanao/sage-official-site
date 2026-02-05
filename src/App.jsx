3d9d9fc7f574d2a8da33bb0ea86678996bbba50c
naoanao <131459006+naoanao@users.noreply.github.com>
2026-02-06 00:36:10 +0900

    Blog: Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
+import Blog from './pages/Blog'
+import BlogPost from './pages/BlogPost'
 import './App.css'
 
 function App() {
-  return <Landing />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
