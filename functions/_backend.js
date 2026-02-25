cdb6dfff7ec2f94dbacdd4a8299c4ec94deb4d08
naoanao <Naofumi0930@gmail.com>
2026-02-25 20:47:15 +0900

    chore: update tunnel URL [https://ddr-pub-los-sold.trycloudflare.com]

--- functions/_backend.js
+++ functions/_backend.js
@@ -1,2 +1,2 @@
-﻿// Auto-updated by run_sage.ps1 on 2026-02-25 20:46:41
-export const BACKEND_URL = "https://biotechnology-cutting-verification-hopkins.trycloudflare.com";
+﻿// Auto-updated by run_sage.ps1 on 2026-02-25 20:47:03
+export const BACKEND_URL = "https://ddr-pub-los-sold.trycloudflare.com";
