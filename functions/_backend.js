273d6d5245a4bd27992290286eb4ae0371038850
naoanao <Naofumi0930@gmail.com>
2026-03-10 12:22:28 +0900

    feat: permanent migration to ngrok static domain

--- functions/_backend.js
+++ functions/_backend.js
@@ -1,2 +1,2 @@
-﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-03-10 12:22:09
+﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-03-10 12:22:21
 export const BACKEND_URL = "https://tetchy-byssal-katherin.ngrok-free.dev";
