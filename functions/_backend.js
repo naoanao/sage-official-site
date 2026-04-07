61ab0423e839e22d7b5de53c778e41b7fa1eaac8
naoanao <Naofumi0930@gmail.com>
2026-04-08 07:50:36 +0900

    chore(ngrok): update backend URL 2026-04-08 07:50

--- functions/_backend.js
+++ functions/_backend.js
@@ -1,2 +1,2 @@
-﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-04-07 08:16:08
+﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-04-08 07:50:25
 export const BACKEND_URL = "https://tetchy-byssal-katherin.ngrok-free.dev";
