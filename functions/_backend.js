074a7731461a9216f7f8c2638ead4ac02fb332e0
naoanao <Naofumi0930@gmail.com>
2026-04-12 10:03:07 +0900

    chore(ngrok): update backend URL 2026-04-12 10:03

--- functions/_backend.js
+++ functions/_backend.js
@@ -1,2 +1,2 @@
-﻿// Auto-updated 2026-04-11
-export const BACKEND_URL = "https://naturally-new-number-receives.trycloudflare.com";
+﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-04-12 10:02:38
+export const BACKEND_URL = "https://pending-ngrok-start";
