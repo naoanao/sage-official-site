06895c6ee56a349888bc018ba4c6c6d2f47bc10a
naoanao <Naofumi0930@gmail.com>
2026-04-22 07:24:36 +0900

    chore(ngrok): update backend URL 2026-04-22 07:24

--- functions/_backend.js
+++ functions/_backend.js
@@ -1,2 +1,2 @@
-﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-04-20 08:35:55
+﻿// Auto-updated by run_sage.ps1 (ngrok) on 2026-04-22 07:24:19
 export const BACKEND_URL = "https://pending-ngrok-start";
